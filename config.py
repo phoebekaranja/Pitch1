d811bbff27fc74f978726153947c836304d301fd
phoebekaranja <phoebemurish@gmail.com>
2018-10-18 14:37:55 +0300

    deployement

--- config.py
+++ config.py
@@ -36,13 +36,14 @@ class ProdConfig(Config):
 class DevConfig(Config):
     '''
     Development  configuration child class
+    
     Args:
         Config: The parent configuration class with General configuration settings
     '''
 
 
     DEBUG = True
-  
+
 
 config_options = {
 'development':DevConfig,
